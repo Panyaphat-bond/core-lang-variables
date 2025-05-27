bb15729d72f40a7fc6e0041dc3476422d2e803e6
panyaphat <panyaphat@panyaphat-s-MacBook-Air.local>
2025-05-27 14:18:43 +0700

    🔧 Add CORS header for API

--- api/lang.ts
+++ api/lang.ts
@@ -2,9 +2,10 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import fs from 'fs';
 import path from 'path';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const filePath = path.join(process.cwd(), 'data', 'system_language_mapping.json');
-  const jsonData = fs.readFileSync(filePath, 'utf-8');
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(jsonData);
-}
+export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // <-- สำคัญ
+    res.setHeader('Content-Type', 'application/json');
+  
+    const data = require('../../core-lang-variables/system_language_mapping.json');
+    res.status(200).json(data);
+  }
